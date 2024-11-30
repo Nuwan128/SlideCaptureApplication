395b4ceab0882b159955d70188452044bc7306d5
Nuwan Pradeep <73054033+Nuwan128@users.noreply.github.com>
2024-11-30 15:14:07 +0530

    Feature/v2 (#7)
    * code-refactor
    
    * finalze v2 changes
    
    * finalze this version

--- SlideCapture/MainForm.Designer.cs
+++ SlideCapture/MainForm.Designer.cs
@@ -32,6 +32,13 @@
             btnStart = new Button();
             btnStop = new Button();
             btnGeneratePDF = new Button();
+            pictureBox1 = new PictureBox();
+            btnMinimize = new Button();
+            btnClose = new Button();
+            SlideCountLabel = new Label();
+            TimeLabel = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // btnStart
@@ -39,52 +46,148 @@
             btnStart.BackColor = Color.Transparent;
             btnStart.BackgroundImage = (Image)resources.GetObject("btnStart.BackgroundImage");
             btnStart.BackgroundImageLayout = ImageLayout.Zoom;
-            btnStart.Location = new Point(12, 12);
+            btnStart.FlatAppearance.BorderSize = 0;
+            btnStart.FlatStyle = FlatStyle.Flat;
+            btnStart.ForeColor = Color.Transparent;
+            btnStart.Location = new Point(97, 18);
             btnStart.Name = "btnStart";
-            btnStart.Size = new Size(80, 40);
-            btnStart.TabIndex = 0;
+            btnStart.Size = new Size(60, 30);
+            btnStart.TabIndex = 1;
             btnStart.UseVisualStyleBackColor = false;
             btnStart.Click += btnStart_Click;
             // 
             // btnStop
             // 
+            btnStop.BackColor = Color.Transparent;
             btnStop.BackgroundImage = (Image)resources.GetObject("btnStop.BackgroundImage");
             btnStop.BackgroundImageLayout = ImageLayout.Zoom;
-            btnStop.Location = new Point(115, 12);
+            btnStop.FlatAppearance.BorderSize = 0;
+            btnStop.FlatStyle = FlatStyle.Flat;
+            btnStop.Location = new Point(167, 18);
             btnStop.Name = "btnStop";
-            btnStop.Size = new Size(80, 40);
-            btnStop.TabIndex = 0;
-            btnStop.UseVisualStyleBackColor = true;
+            btnStop.Size = new Size(60, 30);
+            btnStop.TabIndex = 2;
+            btnStop.UseVisualStyleBackColor = false;
             btnStop.Click += btnStop_Click;
             // 
             // btnGeneratePDF
             // 
+            btnGeneratePDF.BackColor = Color.Transparent;
             btnGeneratePDF.BackgroundImage = (Image)resources.GetObject("btnGeneratePDF.BackgroundImage");
             btnGeneratePDF.BackgroundImageLayout = ImageLayout.Zoom;
-            btnGeneratePDF.Location = new Point(218, 12);
+            btnGeneratePDF.FlatAppearance.BorderSize = 0;
+            btnGeneratePDF.FlatStyle = FlatStyle.Flat;
+            btnGeneratePDF.Location = new Point(237, 18);
             btnGeneratePDF.Name = "btnGeneratePDF";
-            btnGeneratePDF.Size = new Size(80, 40);
-            btnGeneratePDF.TabIndex = 0;
-            btnGeneratePDF.UseVisualStyleBackColor = true;
+            btnGeneratePDF.Size = new Size(60, 30);
+            btnGeneratePDF.TabIndex = 3;
+            btnGeneratePDF.UseVisualStyleBackColor = false;
             btnGeneratePDF.Click += btnGeneratePDF_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(86, 64);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // btnMinimize
+            // 
+            btnMinimize.BackColor = Color.Transparent;
+            btnMinimize.BackgroundImage = (Image)resources.GetObject("btnMinimize.BackgroundImage");
+            btnMinimize.BackgroundImageLayout = ImageLayout.Zoom;
+            btnMinimize.Cursor = Cursors.Hand;
+            btnMinimize.FlatAppearance.BorderSize = 0;
+            btnMinimize.FlatStyle = FlatStyle.Flat;
+            btnMinimize.ForeColor = Color.Transparent;
+            btnMinimize.Location = new Point(373, -3);
+            btnMinimize.Name = "btnMinimize";
+            btnMinimize.Size = new Size(58, 29);
+            btnMinimize.TabIndex = 4;
+            btnMinimize.UseVisualStyleBackColor = true;
+            btnMinimize.Click += btnMinimize_Click;
+            // 
+            // btnClose
+            // 
+            btnClose.BackColor = Color.Transparent;
+            btnClose.BackgroundImage = (Image)resources.GetObject("btnClose.BackgroundImage");
+            btnClose.BackgroundImageLayout = ImageLayout.Zoom;
+            btnClose.Cursor = Cursors.Hand;
+            btnClose.FlatAppearance.BorderSize = 0;
+            btnClose.FlatStyle = FlatStyle.Flat;
+            btnClose.ForeColor = Color.Transparent;
+            btnClose.Location = new Point(437, -3);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(58, 29);
+            btnClose.TabIndex = 5;
+            btnClose.UseVisualStyleBackColor = true;
+            btnClose.Click += btnClose_Click;
+            // 
+            // SlideCountLabel
+            // 
+            SlideCountLabel.AutoSize = true;
+            SlideCountLabel.BackColor = Color.Transparent;
+            SlideCountLabel.ForeColor = Color.FromArgb(143, 40, 198);
+            SlideCountLabel.Location = new Point(300, 33);
+            SlideCountLabel.Name = "SlideCountLabel";
+            SlideCountLabel.Size = new Size(134, 20);
+            SlideCountLabel.TabIndex = 3;
+            SlideCountLabel.Text = "Slide Captured : 00";
+            // 
+            // TimeLabel
+            // 
+            TimeLabel.AutoSize = true;
+            TimeLabel.BackColor = Color.Transparent;
+            TimeLabel.ForeColor = Color.FromArgb(143, 40, 198);
+            TimeLabel.Location = new Point(451, 33);
+            TimeLabel.Name = "TimeLabel";
+            TimeLabel.Size = new Size(44, 20);
+            TimeLabel.TabIndex = 3;
+            TimeLabel.Text = "00:00";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.ForeColor = Color.FromArgb(143, 40, 198);
+            label2.Location = new Point(435, 33);
+            label2.Name = "label2";
+            label2.Size = new Size(15, 20);
+            label2.TabIndex = 3;
+            label2.Text = "/";
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Control;
-            ClientSize = new Size(325, 59);
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(510, 64);
+            ControlBox = false;
+            Controls.Add(label2);
+            Controls.Add(TimeLabel);
+            Controls.Add(SlideCountLabel);
+            Controls.Add(btnClose);
+            Controls.Add(btnMinimize);
+            Controls.Add(pictureBox1);
             Controls.Add(btnGeneratePDF);
             Controls.Add(btnStop);
             Controls.Add(btnStart);
+            FormBorderStyle = FormBorderStyle.None;
             Icon = (Icon)resources.GetObject("$this.Icon");
             MaximizeBox = false;
-            MaximumSize = new Size(343, 106);
-            MinimumSize = new Size(343, 106);
+            MinimizeBox = false;
             Name = "MainForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "SlideCapture_V1";
+            Load += MainForm_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -92,5 +195,11 @@
         private Button btnStart;
         private Button btnStop;
         private Button btnGeneratePDF;
-    }
+        private PictureBox pictureBox1;
+        private Button btnMinimize;
+        private Button btnClose;
+        private Label SlideCountLabel;
+        private Label TimeLabel;
+        private Label label2;
+	}
 }
--- SlideCapture/MainForm.cs
+++ SlideCapture/MainForm.cs
@@ -1,131 +1,279 @@
-﻿namespace SlideCapture;
+﻿using System.Drawing.Drawing2D;
+using System.Runtime.InteropServices;
+
+namespace SlideCapture;
 
 public partial class MainForm : Form
 {
-    private readonly System.Windows.Forms.Timer _captureTimer;
-    private readonly IScreenCapture _screenCapture;
-    private readonly ISlideComparator _slideComparator;
-    private readonly IPDFGenerator _pdfGenerator;
-    private readonly List<Bitmap> _slides;
-    private readonly Rectangle _captureRegion;
-    private ToolTip buttonToolTip;
+	#region Fields
+	// Time tracking for slide capture
+	private int seconds = 0;
+	private int minutes = 0;
 
-    public MainForm(IScreenCapture screenCapture, ISlideComparator slideComparator, IPDFGenerator pdfGenerator)
-    {
-        InitializeComponent();
+	// Timers for slide capture and application events
+	private readonly System.Windows.Forms.Timer _captureTimer;
+	private readonly System.Windows.Forms.Timer _appTimer;
 
-        this.TopMost = true;
-        _screenCapture = screenCapture;
-        _slideComparator = slideComparator;
-        _pdfGenerator = pdfGenerator;
-        _slides = new List<Bitmap>();
+	// Services for screen capture, slide comparison, and PDF generation
+	private readonly IScreenCapture _screenCapture;
+	private readonly ISlideComparator _slideComparator;
+	private readonly IPDFGenerator _pdfGenerator;
 
-        // capture region here
-        _captureRegion = Screen.PrimaryScreen.Bounds;
+	// List of captured slides and the capture region
+	private readonly List<Bitmap> _slides;
+	private readonly Rectangle _captureRegion;
 
-        // capture timer
-        _captureTimer = new System.Windows.Forms.Timer();
-        _captureTimer.Interval = 5000; // Capture every 5 seconds
-        _captureTimer.Tick += CaptureTimer_Tick;
+	// ToolTip for buttons
+	private ToolTip buttonToolTip;
 
-        // ToolTips
-        buttonToolTip = new ToolTip();
-        AddToolTips();
+	// Constants for window style (shadow effect)
+	private const int CS_DROPSHADOW = 0x00020000;
 
-        btnStop.Enabled = false;
+	// Constants for dragging
+	private const int WM_NCLBUTTONDOWN = 0xA1;
+	private const int HTCAPTION = 0x2;
 
-        LogMessage("Application started. Ready to capture slides.");
-    }
+	#endregion
 
-    // Tooltip
-    private void AddToolTips()
-    {
-        buttonToolTip.SetToolTip(btnStart, "Start Slide Capture");
-        buttonToolTip.SetToolTip(btnStop, "Stop Slide Capture");
-        buttonToolTip.SetToolTip(btnGeneratePDF, "Generate PDF of Slides");
-    }
+	#region Win32 API Declarations
+	// External Win32 API for window style manipulation
+	[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+	private static extern int SetClassLong(IntPtr hWnd, int nIndex, int dwNewLong);
 
-    // Capture and compare slides
-    private void CaptureTimer_Tick(object sender, EventArgs e)
-    {
-        Bitmap currentSlide = _screenCapture.CaptureRegion(_captureRegion);
+	[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+	private static extern int GetClassLong(IntPtr hWnd, int nIndex);
 
-        if (!_slideComparator.IsDuplicate(currentSlide))
-        {
-            _slides.Add(currentSlide);
-            LogMessage($"New slide captured. Total slides: {_slides.Count}");
-        }
-        else
-        {
-            LogMessage("Duplicate slide detected. Skipping capture.");
-        }
-    }
+	[DllImport("user32.dll")]
+	public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
 
-    // Log messages
-    private void LogMessage(string message)
-    {
-        Console.WriteLine($"[MainForm] {message}");
-    }
+	[DllImport("user32.dll")]
+	public static extern bool ReleaseCapture();
+	#endregion
 
-    // Start capturing
-    private void btnStart_Click(object sender, EventArgs e)
-    {
-        var result = MessageBox.Show("Are you sure you want to start capturing slides?",
-                                      "Confirm Start", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+	#region Constructor
+	// Constructor initializing necessary services and setting up timers
+	public MainForm(IScreenCapture screenCapture, ISlideComparator slideComparator, IPDFGenerator pdfGenerator)
+	{
+		InitializeComponent();
+		ApplyShadow();  // Apply window shadow effect
+		this.TopMost = true;  // Keep the form always on top
+		this.FormBorderStyle = FormBorderStyle.None; // Remove default borders
+		this.MouseDown += MainForm_MouseDown;
 
-        if (result == DialogResult.Yes)
-        {
-            LogMessage("Slide capturing started.");
-            _captureTimer.Start();
-            btnStop.Enabled = true;
-            btnStart.Enabled = false;
-        }
-    }
+		// Initialize services
+		_screenCapture = screenCapture;
+		_slideComparator = slideComparator;
+		_pdfGenerator = pdfGenerator;
 
-    // Stop capturing
-    private void btnStop_Click(object sender, EventArgs e)
-    {
-        var result = MessageBox.Show("Are you sure you want to stop capturing slides?",
-                                      "Confirm Stop", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+		// Initialize fields
+		_slides = new List<Bitmap>();
+		_captureRegion = Screen.PrimaryScreen.Bounds; // Define capture region to cover the entire screen
 
-        if (result == DialogResult.Yes)
-        {
+		// Initialize timers
+		_captureTimer = new System.Windows.Forms.Timer { Interval = 5000 }; // Capture every 5 seconds
+		_captureTimer.Tick += CaptureTimer_Tick;
+
+		_appTimer = new System.Windows.Forms.Timer { Interval = 1000 }; // 1-second interval for app timer
+		_appTimer.Tick += AppTimer_Tick; // Update time every second
+
+		// Initialize Tooltips for buttons
+		buttonToolTip = new ToolTip();
+		AddToolTips();
+
+		btnStop.Enabled = false; // Disable Stop button initially
+
+		LogMessage("Application started. Ready to capture slides.");
+	}
+	#endregion
+
+	#region UI Event Handlers
+	// Dragable form setup
+	private void MainForm_MouseDown(object? sender, MouseEventArgs e)
+	{
+		if (e.Button == MouseButtons.Left)
+		{
+			ReleaseCapture();
+			SendMessage(this.Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+		}
+	}
+
+	// Start capturing slides on button click
+	private void btnStart_Click(object sender, EventArgs e)
+	{
+		var result = MessageBox.Show("Are you sure you want to start capturing slides?", "Confirm Start", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+		if (result == DialogResult.Yes)
+		{
+			_appTimer.Start();
+			_captureTimer.Start();
+			btnStop.Enabled = true;
+			btnStart.Enabled = false;
+			LogMessage("Slide capturing started.");
+		}
+	}
+
+	// Stop capturing slides on button click
+	private void btnStop_Click(object sender, EventArgs e)
+	{
+		var result = MessageBox.Show("Are you sure you want to stop capturing slides?", "Confirm Stop", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+		if (result == DialogResult.Yes)
+		{
+			_appTimer.Stop();
+			_captureTimer.Stop();
+			btnStart.Enabled = true;
+			btnStop.Enabled = false;
+            btnGeneratePDF.Enabled = true;
+
+            btnGeneratePDF.BackgroundImage = Properties.Resources.Pdf_Download_Default;
+            CustomizeButtonHover(btnGeneratePDF, Properties.Resources.Pdf_Download_Default, Properties.Resources.Pdf_Download_Hover);
+
             LogMessage("Slide capturing stopped.");
-            _captureTimer.Stop();
+		}
+	}
 
-            btnStart.Enabled = true;
-            btnStop.Enabled = false;
-        }
-    }
+	// Generate a PDF from captured slides
+	private void btnGeneratePDF_Click(object sender, EventArgs e)
+	{
+		if (_slides.Count > 0) // Ensure there are slides to generate a PDF
+		{
+			using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+			{
+				saveFileDialog.Filter = "PDF files (*.pdf)|*.pdf";
+				saveFileDialog.Title = "Save PDF File";
+				saveFileDialog.FileName = "LectureSlides.pdf";
 
-    // Generate PDF from captured slides
-    private void btnGeneratePDF_Click(object sender, EventArgs e)
-    {
-        
-        if (_slides.Count > 0)
-        {
-            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
-            {
-                saveFileDialog.Filter = "PDF files (*.pdf)|*.pdf";
-                saveFileDialog.Title = "Save PDF File";
-                saveFileDialog.FileName = "LectureSlides.pdf";
+				if (saveFileDialog.ShowDialog() == DialogResult.OK)
+				{
+					string outputPath = saveFileDialog.FileName;
+					_pdfGenerator.GeneratePDF(_slides, outputPath); // Generate the PDF
+					MessageBox.Show("PDF Generated Successfully: " + outputPath, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+					_slides.Clear(); // Clear slides after PDF generation
 
-                if (saveFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    string outputPath = saveFileDialog.FileName;
+					ResetTimersAndUI();
+					LogMessage("Slides cleared after PDF generation.");
+				}
+			}
+		}
+		else
+		{
+			CustomizeButtonHover(btnGeneratePDF, Properties.Resources.Pdf_Default, Properties.Resources.Pdf_Default_Hover);
+			MessageBox.Show("No slides captured. Please capture slides before generating the PDF.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+		}
+	}
 
-                    _pdfGenerator.GeneratePDF(_slides, outputPath);
-                    MessageBox.Show("PDF Generated Successfully: " + outputPath,
-                                    "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    _slides.Clear();
-                    LogMessage("Slides cleared after PDF generation.");
-                }
-            }
-        }
-        else
-        {
-            MessageBox.Show("No slides captured. Please capture slides before generating the PDF.",
-                            "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-        }
-    }
+	// Minimize the application window
+	private void btnMinimize_Click(object sender, EventArgs e)
+	{
+		this.WindowState = FormWindowState.Minimized;
+	}
+
+	// Close the application window
+	private void btnClose_Click(object sender, EventArgs e)
+	{
+		this.Close();
+	}
+
+	private void MainForm_Load(object sender, EventArgs e)
+	{
+		CustomizeButtonHover(btnStart, Properties.Resources.Start_Default, Properties.Resources.Start_Hover);
+		CustomizeButtonHover(btnStop, Properties.Resources.Stop_Default, Properties.Resources.Stop_Hover);
+		CustomizeButtonHover(btnGeneratePDF, Properties.Resources.Pdf_Default, Properties.Resources.Pdf_Default_Hover);
+	}
+	#endregion
+
+	#region Private Helper Methods
+	// Apply shadow effect to the form
+	private void ApplyShadow()
+	{
+		int classStyle = GetClassLong(this.Handle, -200);
+		SetClassLong(this.Handle, -200, classStyle | CS_DROPSHADOW);
+	}
+
+	// Set rounded corners for the form's border
+	protected override void OnPaint(PaintEventArgs e)
+	{
+		base.OnPaint(e);
+
+		int borderRadius = 5;
+		GraphicsPath path = new GraphicsPath();
+
+		// Add rounded corners to the form
+		path.AddArc(new Rectangle(0, 0, borderRadius * 2, borderRadius * 2), 180, 90);
+		path.AddArc(new Rectangle(this.Width - borderRadius * 2, 0, borderRadius * 2, borderRadius * 2), 270, 90);
+		path.AddArc(new Rectangle(this.Width - borderRadius * 2, this.Height - borderRadius * 2, borderRadius * 2, borderRadius * 2), 0, 90);
+		path.AddArc(new Rectangle(0, this.Height - borderRadius * 2, borderRadius * 2, borderRadius * 2), 90, 90);
+
+		path.CloseAllFigures();
+
+		this.Region = new Region(path);
+	}
+
+	// Add Tooltips to buttons
+	private void AddToolTips()
+	{
+		buttonToolTip.SetToolTip(btnStart, "Start Slide Capture");
+		buttonToolTip.SetToolTip(btnStop, "Stop Slide Capture");
+		buttonToolTip.SetToolTip(btnGeneratePDF, "Generate PDF of Slides");
+	}
+
+	// Timer to update the elapsed time displayed on the form
+	private void AppTimer_Tick(object? sender, EventArgs e)
+	{
+		seconds++;
+		if (seconds >= 60)
+		{
+			seconds = 0;
+			minutes++;
+		}
+
+		TimeLabel.Text = $"{minutes:00}:{seconds:00}";
+	}
+
+	// Timer that captures the screen and compares slides
+	private void CaptureTimer_Tick(object sender, EventArgs e)
+	{
+		Bitmap currentSlide = _screenCapture.CaptureRegion(_captureRegion);
+
+		if (!_slideComparator.IsDuplicate(currentSlide)) // Check if the slide is new
+		{
+			_slides.Add(currentSlide); // Add the slide to the list
+			SlideCountLabel.Text = $"Slide Captured : {_slides.Count:00}";
+			LogMessage($"New slide captured. Total slides: {_slides.Count}");
+		}
+		else
+		{
+			LogMessage("Duplicate slide detected. Skipping capture.");
+		}
+	}
+
+	// Log messages to the console (for debugging or tracking)
+	private void LogMessage(string message)
+	{
+		Console.WriteLine(message); // You can also log to a file if necessary
+	}
+
+	// Reset timers and UI to default state
+	private void ResetTimersAndUI()
+	{
+        btnGeneratePDF.BackgroundImage = Properties.Resources.Pdf_Default;
+        CustomizeButtonHover(btnGeneratePDF, Properties.Resources.Pdf_Default, Properties.Resources.Pdf_Default_Hover);
+        _appTimer.Stop();
+		_captureTimer.Stop();
+		seconds = 0;
+		minutes = 0;
+		TimeLabel.Text = "00:00";
+		SlideCountLabel.Text = "Slide Captured : 00";
+	}
+
+	// Customize the appearance of buttons on hover
+	private void CustomizeButtonHover(Button button, Image defaultImage, Image hoverImage)
+	{
+		button.FlatAppearance.MouseOverBackColor = Color.Transparent;
+		button.FlatAppearance.MouseDownBackColor = Color.Transparent;
+
+		button.MouseEnter += (sender, e) => button.BackgroundImage = hoverImage;
+		button.MouseLeave += (sender, e) => button.BackgroundImage = defaultImage;
+	}
+	#endregion
 }
